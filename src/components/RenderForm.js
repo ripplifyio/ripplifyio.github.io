6c6a3a9d2ed32d6fb6c83e0bbe96488579d5bdc8
ErikBoesen <me@erikboesen.com>
2023-08-11 19:57:18 -0400

    Clean up options content

--- src/components/RenderForm.js
+++ src/components/RenderForm.js
@@ -5,14 +5,17 @@ import { render }  from '../services/API';
 
 const RenderForm = ({ setGraphImage }) => {
     const [state, setState] = useState({
-        chartType: 'stacked',
+        chartType: 'river',
         colorScheme: 'rainbow',
         silouhette: true,
+        artistCount: 100,
+        excludedArtists: '',
     });
+    const [changed, setChanged] = useState(true);
 
     const handleChange = (event) => {
         setState({
-            [event.target.name]: event.target.type == 'checkbox'
+            [event.target.name]: event.target.type === 'checkbox'
                                     ? event.target.checked
                                     : event.target.value,
         });
@@ -29,17 +32,17 @@ const RenderForm = ({ setGraphImage }) => {
         <form className='options' onSubmit={handleSubmit}>
             <div className='column'>
                 <div className='option'>
-                    <p>Chart type</p>
-                    <div className='radioGroup' onChange={handleChange}>
-                        <label><input type='radio' name='chartType' value='ratio' /> Ratio</label>
-                        <label><input type='radio' name='chartType' value='river' /> River</label>
-                    </div>
+                    <label htmlFor='chartType'>Chart type</label>
+                    <select name='chartType' onChange={handleChange}>
+                        <option value='river'>River</option>
+                        <option value='stretch'>Stretch</option>
+                    </select>
                 </div>
                 <div className='option'>
                     <label htmlFor='colorScheme'>Color Scheme</label>
                     <select name='colorScheme' id='colorScheme' onChange={handleChange}>
-                        <option value='classic'>Classic</option>
                         <option value='rainbow'>Rainbow</option>
+                        <option value='classic'>Classic</option>
                     </select>
                 </div>
                 {/*
@@ -53,26 +56,28 @@ const RenderForm = ({ setGraphImage }) => {
                     </select>
                 </div>
                 */}
+            </div>
+            <div className='column'>
                 <div className='option'>
                     <label>
                         <input type='checkbox' name='sillouhette' checked onChange={handleChange} />
-                        Force graph to weight to center?
+                        Weight graph to center?
                     </label>
                 </div>
                 <div className='option'>
                     <label>
                         Number of artists to include?
-                        <input type='number' name='artistCount' id='artistCount' defaultValue={100} onChange={handleChange} />
+                        <input type='range' min={20} max={500} name='artistCount' id='artistCount' defaultValue={100} onChange={handleChange} />
                     </label>
                 </div>
                 <div className='option'>
                     <label>
-                        Artist names to exclude (one per line)
-                        <textarea name='excludedArtists' id='excludedArtists'></textarea>
+                        Artists to hide (comma separated)
+                        <input type='text' name='excludedArtists' id='excludedArtists' />
                     </label>
                 </div>
-                <input type="submit" value="Generate Graph" />
             </div>
+            <input type='submit' value='Generate Graph' />
         </form>
     );
 };
