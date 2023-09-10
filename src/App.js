3f606dc1e3ba227d2dd71f9ba22f866526fdb899
ErikBoesen <me@erikboesen.com>
2023-09-10 10:32:40 -0400

    Add more code for token management

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectHistoryFiles, fetchHistoryFiles } from './slice';
 
 import './App.css';
 //import * as Globals from './Globals';
@@ -12,27 +14,37 @@ import Splash from './pages/Splash';
 import Guide from './pages/Guide';
 
 function App() {
+    const dispatch = useDispatch();
+    const historyFiles = useSelector(selectHistoryFiles);
+
     const [token, setToken] = useState(null);
-    const [user, setUser] = useState(null);
+
+    const load = () => {
+        dispatch(fetchHistoryFiles);
+    };
 
     useEffect(() => {
         const hash = window.location.hash;
         let token = localStorage.getItem('token');
 
-        if (!token && hash) {
-            const spotifyToken = hash.substring(1).split('&').find(elem => elem.startsWith('access_token')).split('=')[1];
-            console.log('Spotify token:', spotifyToken);
-            authorize(spotifyToken)
-                .then((data) => {
-                    console.log(data);
-                    const { user, token } = data;
-                    console.log('Our token:', token);
+        if (token) {
+            load();
+        } else {
+            if (hash) {
+                const spotifyToken = hash.substring(1).split('&').find(elem => elem.startsWith('access_token')).split('=')[1];
+                console.log('Spotify token:', spotifyToken);
+                authorize(spotifyToken)
+                    .then((data) => {
+                        console.log(data);
+                        const { token } = data;
+                        console.log('Our token:', token);
 
-                    window.location.hash = '';
-                    localStorage.setItem('token', token);
-                    localStorage.setItem('user', user);
-                    setToken(token);
-                });
+                        window.location.hash = '';
+                        localStorage.setItem('token', token);
+                        setToken(token);
+                        load();
+                    });
+            }
         }
 
         setToken(token);
@@ -40,27 +52,28 @@ function App() {
 
     const logout = () => {
         setToken(null);
-        setUser(null);
         localStorage.removeItem('token');
-        localStorage.removeItem('user');
     };
 
     const getUser = () => {
+
     };
 
     return (
-        <div className='App'>
-            <header>
-                <Logo />
-                <h1>Ripplify</h1>
-            </header>
-            {!token
-                ? <Splash />
-                : <Guide />
-            }
-            <Viewer example={!Boolean(token)} />
-            <Navbar isLoggedIn={Boolean(token)} logout={logout} />
-        </div>
+        <Provider store={store}>
+            <div className='App'>
+                <header>
+                    <Logo />
+                    <h1>Ripplify</h1>
+                </header>
+                {!token
+                    ? <Splash />
+                    : <Guide />
+                }
+                <Viewer example={!Boolean(token)} />
+                <Navbar isLoggedIn={Boolean(token)} logout={logout} />
+            </div>
+        </Provider>
     );
 }
 
