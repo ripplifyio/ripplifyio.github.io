017db9a48d5e71c88dd223d15a3864c37bf913c5
ErikBoesen <me@erikboesen.com>
2023-10-12 00:11:44 -0400

    Tweak homepage styling

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -6,7 +6,7 @@ const Splash = () => {
     return (
         <div className='splash'>
             <h2>Create stunning visualizations of your Spotify listening history.</h2>
-            <a className='button' href={`${Globals.AUTH_ENDPOINT}?client_id=${Globals.CLIENT_ID}&redirect_uri=${Globals.REDIRECT_URI}&response_type=${Globals.RESPONSE_TYPE}&scope=${Globals.SCOPE}`}>Login with Spotify</a>
+            <a className='button big' href={`${Globals.AUTH_ENDPOINT}?client_id=${Globals.CLIENT_ID}&redirect_uri=${Globals.REDIRECT_URI}&response_type=${Globals.RESPONSE_TYPE}&scope=${Globals.SCOPE}`}>Login with Spotify</a>
             <img className='exampleImage' alt='Example graph of your music listening history' src={exampleGraphImage} />
         </div>
     );
