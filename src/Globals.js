17401273ca23e7496bcf932e6c20ab92dfb0fee0
ErikBoesen <me@erikboesen.com>
2023-10-11 21:27:47 -0400

    Fix redirect URI scheme

--- src/Globals.js
+++ src/Globals.js
@@ -1,6 +1,6 @@
 export const CLIENT_ID = '9e5ee3cb761a4c1097521541c43e8799';
 //export const REDIRECT_URI = 'http://localhost:3000';
-export const REDIRECT_URI = 'http://ripplify.io';
+export const REDIRECT_URI = 'https://ripplify.io';
 export const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
 export const SCOPE = ['user-read-email'].join(' ');
 export const RESPONSE_TYPE = 'token';
