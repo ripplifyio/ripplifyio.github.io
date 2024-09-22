fbe481946becd7acbd74c310a30329277a0f567c
ErikBoesen <me@erikboesen.com>
2024-09-22 15:42:15 -0400

    Add option group

---
+++ src/components/OptionGroup.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
+
+const OptionGroup = ({ title, children }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <div className='option-group'>
+      <div className='option-group-header' onClick={toggleExpand}>
+        <h3>{title}</h3>
+        <FontAwesomeIcon icon={isExpanded ? faChevronUp : faChevronDown} />
+      </div>
+      {isExpanded && (
+        <div className='option-group-content'>
+          {children}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default OptionGroup;
