104238ef57c5571e87115f788d702ee61cb198cb
ErikBoesen <me@erikboesen.com>
2024-09-24 00:12:42 -0400

    Make login page update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { sendMagicLink } from '../services/API';
 
 const Login = () => {
-
+    const [email, setEmail] = useState(null);
+    const [submitted, setSubmitted] = useState(false);
 
     return (
         <div className='Login'>
             <h2>Log in or register</h2>
-            <p>Enter the email you use for Spotify.</p>
-            <input type='email' name='email' placeholder='Email' />
-            <a className='button' onClick={}>Next</a>
+            {submitted ? (
+                <>
+                    <p>Check your email and click the link you were sent to finish logging in.</p>
+                </>
+            ) : (
+                <>
+                    <p>Enter the email you use for Spotify.</p>
+                    <input type='email' placeholder='Email' id='emailInput' onInput={setEmail} />
+                    <a className='button' onClick={() => {
+                        sendMagicLink(email).then(() => setSubmitted(true));
+                    }}>Next</a>
+                </>
+            )}
         </div>
     );
 };
